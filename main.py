89ea15654c61be8fcf739cd85929cc5bab81f7a6
Lingjia007 <3125497423@qq.com>
2026-03-17 18:06:15 +0800

    1

---
+++ main.py
@@ -0,0 +1,79 @@
+# coding:utf-8
+import sys
+import os
+
+from PyQt5.QtCore import Qt, QUrl, QLocale, QTranslator
+from PyQt5.QtGui import QIcon, QDesktopServices
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
+from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, Theme, FluentWindow,
+                            NavigationAvatarWidget, SubtitleLabel, setFont, InfoBadge,
+                            InfoBadgePosition, FluentTranslator)
+from qfluentwidgets import FluentIcon as FIF
+from settings.setting_interface import SettingInterface
+from settings.config import cfg, Language
+
+
+class Widget(QWidget):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        self.label = SubtitleLabel(text, self)
+        self.hBoxLayout = QHBoxLayout(self)
+
+        setFont(self.label, 24)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class Window(FluentWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.homeInterface = Widget('Home Interface', self)
+        self.settingInterface = SettingInterface(self)
+
+        self.initNavigation()
+        self.initWindow()
+
+    def initNavigation(self):
+        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
+        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+    def initWindow(self):
+        self.resize(900, 700)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('IAP Host Computer')
+
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+
+
+if __name__ == '__main__':
+    if cfg.get(cfg.dpiScale) == "Auto":
+        QApplication.setHighDpiScaleFactorRoundingPolicy(
+            Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    else:
+        os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+        os.environ["QT_SCALE_FACTOR"] = str(cfg.get(cfg.dpiScale))
+
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
+
+    locale = cfg.get(cfg.language).value
+    fluentTranslator = FluentTranslator(locale)
+    settingTranslator = QTranslator()
+    settings_i18n_path = os.path.join(os.path.dirname(__file__), 'settings', 'resource', 'i18n')
+    settingTranslator.load(locale, "settings", ".", settings_i18n_path)
+
+    app.installTranslator(fluentTranslator)
+    app.installTranslator(settingTranslator)
+
+    w = Window()
+    w.show()
+    app.exec_()
---
+++ settings/__init__.py
@@ -0,0 +1,3 @@
+# coding:utf-8
+from .setting_interface import SettingInterface
+from .config import cfg, HELP_URL, FEEDBACK_URL, AUTHOR, VERSION, YEAR
